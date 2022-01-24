8e90f4aedfaaaf6da9ccd494ba75a9b60ebef40c
Efim Reutov <efimreutov@gmail.com>
2022-01-24 17:21:01 +0200

    fixed errors and added comments

--- config/config.go
+++ config/config.go
@@ -39,15 +39,15 @@ type Configuration struct {
 func LoadCfg() (*Configuration, error) {
 	env := os.Getenv("ENV")
 	if env == "" {
-		return nil, errors.New("$ENV is empty")
+		return nil, errors.New("the required $ENV environment variable is missing")
 	}
 	serviceHost := os.Getenv("SERVICE_HOST")
 	if env == "" {
-		return nil, errors.New("SERVICE_HOST is empty")
+		return nil, errors.New("the required $SERVICE_HOST environment variable is missing")
 	}
 	servicePort := os.Getenv("SERVICE_PORT")
 	if servicePort == "" {
-		return nil, errors.New("SERVICE_PORT is empty")
+		return nil, errors.New("the required $SERVICE_PORT environment variable is missing")
 	}
 	servicePortInt, err := strconv.Atoi(servicePort)
 	if err != nil {
@@ -55,27 +55,27 @@ func LoadCfg() (*Configuration, error) {
 	}
 	serverType := os.Getenv("SERVER_TYPE")
 	if serverType == "" {
-		return nil, errors.New("SERVER_TYPE is empty")
+		return nil, errors.New("the required $SERVER_TYPE environment variable is missing")
 	}
 	postgresDB := os.Getenv("POSTGRES_DB")
 	if postgresDB == "" {
-		return nil, errors.New("$POSTGRES_DB is empty0")
+		return nil, errors.New("$the required $POSTGRES_DB environment variable is missing")
 	}
 	postgresUser := os.Getenv("POSTGRES_USER")
 	if postgresUser == "" {
-		return nil, errors.New("POSTGRES_USER is empty")
+		return nil, errors.New("the required $POSTGRES_USER environment variable is missing")
 	}
 	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
 	if postgresPassword == "" {
-		return nil, errors.New("POSTGRES_PASSWORD is empty")
+		return nil, errors.New("the required $POSTGRES_PASSWORD environment variable is missing")
 	}
 	postgresHost := os.Getenv("POSTGRES_HOST")
 	if postgresHost == "" {
-		return nil, errors.New("POSTGRES_HOST is empty")
+		return nil, errors.New("the required $POSTGRES_HOST environment variable is missing")
 	}
 	postgresPort := os.Getenv("POSTGRES_PORT")
 	if postgresPort == "" {
-		return nil, errors.New("POSTGRES_PORT is empty")
+		return nil, errors.New("the required $POSTGRES_PORT environment variable is missing")
 	}
 	postgresPortInt, err := strconv.Atoi(postgresPort)
 	if err != nil {
@@ -83,11 +83,11 @@ func LoadCfg() (*Configuration, error) {
 	}
 	postgresSSLMode := os.Getenv("POSTGRES_SSL_MODE")
 	if postgresSSLMode == "" {
-		return nil, errors.New("POSTGRES_SSL_MODE is empty")
+		return nil, errors.New("the required $POSTGRES_SSL_MODE environment variable is missing")
 	}
 	smtpServer := os.Getenv("SMTP_SERVER")
 	if smtpServer == "" {
-		return nil, errors.New("SMTP_SERVER is empty")
+		return nil, errors.New("the required $SMTP_SERVER environment variable is missing")
 	}
 	smtpPort := os.Getenv("SMTP_PORT")
 	smtpPortInt, err := strconv.Atoi(smtpPort)
@@ -95,15 +95,15 @@ func LoadCfg() (*Configuration, error) {
 		return nil, err
 	}
 	if smtpPort == "" {
-		return nil, errors.New("SMTP_PORT is empty")
+		return nil, errors.New("the required $SMTP_PORT environment variable is missing")
 	}
 	mailUser := os.Getenv("MAIL_USER")
 	if mailUser == "" {
-		return nil, errors.New("MAIL_USER is empty")
+		return nil, errors.New("the required $MAIL_USER environment variable is missing")
 	}
 	mailPassword := os.Getenv("MAIL_PASSWORD")
 	if mailPassword == "" {
-		return nil, errors.New("MAIL_PASSWORD is empty")
+		return nil, errors.New("the required $MAIL_PASSWORD environment variable is missing")
 	}
 	return &Configuration{
 		Env:              Env(env),
